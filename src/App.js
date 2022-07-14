3ec5418e8454d49ca97ee19e798eb83fff598285
mjha2475 <73065205+mjha2475@users.noreply.github.com>
2022-07-14 23:15:03 +0530

    Movie Suggestions

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
+import React ,{useState,useEffect} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import MovieList from './MovieList'
 import './App.css';
+import MovieListHeading from './MovieListHeading';
+import SearchBox from './SearchBox';
+import AddFavourites from './AddFavourites';
+
 
 function App() {
+  const[movies,setMovies]=useState([]);
+  const[searchValue,setSearchValue]=useState('');
+  const[favourites,setFavourites]=useState([]);
+  const getMovieRequest=async(searchValue)=>{
+    const url=`http://www.omdbapi.com/?s=${searchValue}&apiKey=5b6548ba`
+    const response=await fetch(url);
+    const responseJson=await response.json();
+  
+    if(responseJson.Search){
+      setMovies(responseJson.Search);
+    }
+  }
+  useEffect(()=>{
+    getMovieRequest(searchValue);
+
+  },[searchValue]);
+
+  const AddFavouriteMovie=(movie)=>{
+    const newFavouriteList=[...AddFavourites,movie];
+    setFavourites(newFavouriteList);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-fluid-movie-app">
+      <div className="row d-flex align-items-center mt-4 mb-4">
+         <MovieListHeading heading="Movies"/>
+         <SearchBox searchValue={searchValue} setSearchValue={setSearchValue}/>
+      </div>
+     <div className='row'>
+      <MovieList 
+      movies={movies}
+      handleFavouritesClick={AddFavouriteMovie}
+      favouriteComponent={AddFavourites}
+      />
+     </div>
+     <div className='row d-flex align-items-center mt-4 mb-4'>
+       <MovieListHeading heading="Favourites"/>
+     </div>
+     <div className="row">
+       <MovieList
+        movies={favourites}
+        
+       />
+     </div>
+      
     </div>
   );
 }
