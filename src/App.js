9eb56c97c0c017fc32f6ac72ff4168af582ebeb2
mjha2475 <73065205+mjha2475@users.noreply.github.com>
2022-07-15 14:17:38 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -5,55 +5,82 @@ import './App.css';
 import MovieListHeading from './MovieListHeading';
 import SearchBox from './SearchBox';
 import AddFavourites from './AddFavourites';
+import RemoveFavourites from './RemoveFavourites';
 
+const App = () => {
+	const [movies, setMovies] = useState([]);
+	const [favourites, setFavourites] = useState([]);
+	const [searchValue, setSearchValue] = useState('');
 
-function App() {
-  const[movies,setMovies]=useState([]);
-  const[searchValue,setSearchValue]=useState('');
-  const[favourites,setFavourites]=useState([]);
-  const getMovieRequest=async(searchValue)=>{
-    const url=`http://www.omdbapi.com/?s=${searchValue}&apiKey=5b6548ba`
-    const response=await fetch(url);
-    const responseJson=await response.json();
-  
-    if(responseJson.Search){
-      setMovies(responseJson.Search);
-    }
-  }
-  useEffect(()=>{
-    getMovieRequest(searchValue);
+	const getMovieRequest = async (searchValue) => {
+		const url = `http://www.omdbapi.com/?s=${searchValue}&apiKey=5b6548ba`;
 
-  },[searchValue]);
+		const response = await fetch(url);
+		const responseJson = await response.json();
 
-  const AddFavouriteMovie=(movie)=>{
-    const newFavouriteList=[...AddFavourites,movie];
-    setFavourites(newFavouriteList);
-  }
-  return (
-    <div className="container-fluid-movie-app">
-      <div className="row d-flex align-items-center mt-4 mb-4">
-         <MovieListHeading heading="Movies"/>
-         <SearchBox searchValue={searchValue} setSearchValue={setSearchValue}/>
-      </div>
-     <div className='row'>
-      <MovieList 
-      movies={movies}
-      handleFavouritesClick={AddFavouriteMovie}
-      favouriteComponent={AddFavourites}
-      />
-     </div>
-     <div className='row d-flex align-items-center mt-4 mb-4'>
-       <MovieListHeading heading="Favourites"/>
-     </div>
-     <div className="row">
-       <MovieList
-        movies={favourites}
-        
-       />
-     </div>
-      
-    </div>
-  );
-}
+		if (responseJson.Search) {
+			setMovies(responseJson.Search);
+		}
+	};
+
+	useEffect(() => {
+		getMovieRequest(searchValue);
+	}, [searchValue]);
+
+	useEffect(() => {
+		const movieFavourites = JSON.parse(
+			localStorage.getItem('react-movie-app-favourites')
+		);
+
+		if (movieFavourites) {
+			setFavourites(movieFavourites);
+		}
+	}, []);
+
+	const saveToLocalStorage = (items) => {
+		localStorage.setItem('react-movie-app-favourites', JSON.stringify(items));
+	};
+
+	const addFavouriteMovie = (movie) => {
+		const newFavouriteList = [...favourites, movie];
+		setFavourites(newFavouriteList);
+		saveToLocalStorage(newFavouriteList);
+	};
+
+	const removeFavouriteMovie = (movie) => {
+		const newFavouriteList = favourites.filter(
+			(favourite) => favourite.imdbID !== movie.imdbID
+		);
+
+		setFavourites(newFavouriteList);
+		saveToLocalStorage(newFavouriteList);
+	};
+
+	return (
+		<div className='container-fluid movie-app'>
+			<div className='row d-flex align-items-center mt-4 mb-4'>
+				<MovieListHeading heading='Movies' />
+				<SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
+			</div>
+			<div className='row'>
+				<MovieList
+					movies={movies}
+					handleFavouritesClick={addFavouriteMovie}
+					favouriteComponent={AddFavourites}
+				/>
+			</div>
+			<div className='row d-flex align-items-center mt-4 mb-4'>
+				<MovieListHeading heading='Favourites' />
+			</div>
+			<div className='row'>
+				<MovieList
+					movies={favourites}
+					handleFavouritesClick={removeFavouriteMovie}
+					favouriteComponent={RemoveFavourites}
+				/>
+			</div>
+		</div>
+	);
+};
 
 export default App;
--- src/MovieList.js
+++ src/MovieList.js
@@ -6,10 +6,10 @@ const MovieList = (props) => {
     <>
        
             {props.movies.map((movie,index) => (
-                <div className="image-container d-flex justify-content-start m3">
+                <div className="image-container d-flex justify-content-start m-3">
                     <img src={movie.Poster} alt="movie"/>
                     <div
-                      onClick={()=>props.handleClick(movie)}
+                      onClick={()=>props.handleFavouritesClick(movie)}
                       className="overlay d-flex align-items-center justify-content-center"
                     >
                       <FavouriteComponent/>
